9138b9b2674a81b62929d4395961f47fea00c59a
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-01-26 14:50:48 -0500

    created photo model in database

---
+++ db/migrate/20160126194423_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.string :photographer
+      t.string :title
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
