bdc93fecc1e9789ff352345184ba495f0ebc5548
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-01-26 15:20:30 -0500

    save new photo into database

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,22 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:photographer, :title, :url)
+  end
 end
